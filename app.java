188b9f64dd5ca3065d2cdc144e06c82ad683afca
surya <surya@devops.com>
2026-08-30 04:42:24 +0000

    java file added

---
+++ app.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Mowa!");
+        System.out.println("Welcome to DevOps Journey");
+    }
+}
